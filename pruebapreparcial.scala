1fc6f575eb4b1d9d118cff65b16eb4a409b8f293
Agustin Heredia Urb <agustin.heredia.urbinatti@mi.unc.edu.ar>
2026-04-08 22:59:03 -0300

    Probando funciones esenciales de scala para el examen del lab

---
+++ pruebapreparcial.scala
@@ -0,0 +1,20 @@
+object Main {
+// BLOQUE 1 — Pattern Matching (base del parcial)
+    type Post = (String, String, String, String, Int)  // (subreddit, title, selftext, formattedDate, score)
+
+    def getScore(p: Post): Int = p.match {
+        case (_, _, _, _, score) => score
+    }
+    def isHighScore(p: Post): Boolean = 
+        getScore(p) > 100
+    def summarizePost(p: Post): String = p match {
+        case (subreddit, title, _, _, score) => s"[$subreddit] $title ($score)"
+    }
+// BLOQUE 2 — map / filter / flatMap
+    def titles(posts: List[Post]): List[String] =
+        posts.map { case (_, title, _, _, _) => title }
+    def highScorePosts(posts: List[Post]): List[Post] =
+
+}
+
+
